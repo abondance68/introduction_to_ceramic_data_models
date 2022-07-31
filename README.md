03c65d11c9c2cf8f0ce005d3981556431890aa4c
abondance68 <laurentknauss@gmail.com>
2022-07-31 17:28:58 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+
+**# Project : A demo about Ceramic Network Data Model**
+
+Ceramic Network is a Peer to Peer Network for Composable Data 
+Ceramic Network makes it possible for developpers to build data-rich We3 apps without the need for a traditional database(nosql, etc...) and , in the process unlioks data composability and portability accross every app within the web3 ecosustem.
+
+Apps connect to Ceramic deventralized data nertwork via an open API to: 
+        ..*Store data
+        ..*Retrieve data
+        ..*Modify data
+
+
+
+
+
+## Install packages
+
+
+_yarn  or npm install_
+
+
+## Run the project
+
+
+_yarn dev or npm run dev_
+
+:octocat: 
---
+++ index.js
@@ -0,0 +1,38 @@
+import { CeramicClient } from "@ceramicnetwork/http-client";
+import { DID  } from 'dids'; 
+import { DataModel } from "@glazed/datamodel";
+import { DIDDataStore } from "@glazed/did-datastore";
+import { Ed25519Provider } from "key-did-provider-ed25519";
+import { getResolver } from "key-did-resolver";
+import {fromString} from 'uint8arrays'; 
+import modelAliases from './model.json' assert {type: 'json'}; 
+
+
+const key = fromString(process.env.DID_KEY, "base16"); 
+
+const did = new DID({ 
+    provider:  new Ed25519Provider(key),
+    resolver: getResolver(), 
+
+});
+
+await did.authenticate(); 
+
+const ceramic = new CeramicClient("http://0.0.0.0:7007");
+ceramic.did = did; 
+
+const model = new DataModel({ ceramic, model: modelAliases}); 
+const store = new DIDDataStore({ceramic, model}); 
+
+const person1 = { 
+    name: "Patrick doe",
+    age: 53,
+    friends: [{ name: "Jane Doe", age:26}],
+}; 
+
+const newNote = await model.createTile("Person", person1); 
+
+console.log(newNote.content);
+
+
+
