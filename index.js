554812ac092ac5d489947ca3b2087d9d6ec0c590
abondance68 <laurentknauss@gmail.com>
2022-08-01 16:35:42 +0200

    added eslint

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import { DataModel } from "@glazed/datamodel";
 import { DIDDataStore } from "@glazed/did-datastore";
 import { Ed25519Provider } from "key-did-provider-ed25519";
 import { getResolver } from "key-did-resolver";
-import {fromString} from 'uint8arrays'; 
-import modelAliases from './model.json' assert {type: 'json'}; 
+import {fromString} from 'uint8arrays';
+import modelAliases from './model.json' assert {type: 'json'};
 
 
 const key = fromString(process.env.DID_KEY, "base16"); 
